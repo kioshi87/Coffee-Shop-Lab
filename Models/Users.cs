b3d1bedf75cb68bd8cd27875662984dd5a2f0cc7
Kioshi <kioshimneal@msn.com>
2019-07-29 18:44:37 -0400

    Coffee Shop Lab

---
+++ Models/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace CoffeeShop.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter a UserName")]
+        [StringLength(20, MinimumLength = 5)]
+        [DisplayName("Username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please Enter a Password")]
+        [StringLength(20, MinimumLength = 6)]
+        [DisplayName("Password" )]
+        public string Password { get; set; }
+    }
+}
